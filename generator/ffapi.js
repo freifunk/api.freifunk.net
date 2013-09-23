f6863a7390a37af61f0b7d27189181a8167f9b19
ratopi <ratopi@abwesend.de>
2013-09-23 23:33:03 +0200

    completely refactored code. added method for taking existing JSON into form.

--- generator/ffapi.js
+++ generator/ffapi.js
@@ -1,59 +1,113 @@
-$.getJSON("0.1.json", function(schema) {
-	console.log(schema); // this will show the info it in firebug console
-	schema.form = [
-		"name",
-		"metacommunity",
-		"url",
-		"state",
-		{
-			"type": "fieldset",
-			"expanded": true,
-			"items": "location"
-		},
-		{
-			"type": "fieldset",
-			"title":"Contact (one required)",
-			"expandable": true,
-			"items": "contact"
-		},
-		{
-			"type": "fieldset",
-			"title":"Events",
-			"expandable": true,
-			"items": "events"
-		},
-		{
-			"type": "fieldset",
-			"title":"Feeds",
-			"expandable": true,
-			"items": "feeds"
-		},
-		{
-			"type": "fieldset",
-			"title":"Technical Details",
-			"expandable": true,
-			"items": "techDetails"
-		},
+var handleSchema = function()
+{
+	var currentSchema;
 
-		{
-			"key": "api",
-			"type": "hidden"
-		},
-		{
-			"type": "submit",
-			"title": "OK - give it to me!"
-		}
-	];
-	schema.onSubmit =  function (errors, values) {
-		if (errors) {
-			$('#res').html('<p>I beg your pardon? '+ JSON.stringify(errors) + '</p>');
-		}
-		else {
-			$('#res').html('Hello ' + values.name + '. This is your API file. Place it on a public webserver and add the URL to our directory.<br/><br/>' +
-				(values.age ? '<br/>You are ' + values.age + '.' : '') +
-				'<textarea row=\"288\" cols=\"50\" style=\"margin: 0px 0px 10px; width: 100%; height: 500px;\">' + JSON.stringify(values, null, '  ') + '</textarea>');
-		}
+	// --
+
+	$( '.autohide' ).hide();
+
+	// ---
+
+	var showError =
+		function ( title, message ) {
+			$( '#error' ).show();
+			$( '#error .message' ).text( title );
+			$( '#error .details' ).text( message );
+		};
+
+	// ---
+
+	var takeJson =
+		function() {
+			$( '.autohide' ).hide();
+			var json = $( '#jsonText' ).val();
+			try {
+				currentSchema.value = JSON.parse( json );
+				$('form').empty().jsonForm( currentSchema );
+			}
+			catch ( e ) {
+				showError( "JSON Syntax Error", e );
+			}
+		};
+
+	// ---
+
+	$( '#jsonTakeButton').bind( 'click', takeJson );
+
+	// ---
+
+	var handleSubmit =
+		function (errors, values) {
+	        $( '.autohide' ).hide();
+	        if (errors) {
+	            showError( 'I beg your pardon?', JSON.stringify( errors ) );
+	        }
+	        else {
+	            $( '#result .message' ).show().text( 'Hello ' + values.name + '. This is your API file. Place it on a public webserver and add the URL to our directory.' );
+	            $( '#jsonText' ).val( JSON.stringify( values, null, '  ' ) );
+	        }
+	    };
+
+	// ---
+
+	var formTemplate = [
+        "name",
+        "metacommunity",
+        "url",
+        "state",
+        {
+            "type": "fieldset",
+            "expanded": true,
+            "items": "location"
+        },
+        {
+            "type": "fieldset",
+            "title":"Contact (one required)",
+            "expandable": true,
+            "items": "contact"
+        },
+        {
+            "type": "fieldset",
+            "title":"Events",
+            "expandable": true,
+            "items": "events"
+        },
+        {
+            "type": "fieldset",
+            "title":"Feeds",
+            "expandable": true,
+            "items": "feeds"
+        },
+        {
+            "type": "fieldset",
+            "title":"Technical Details",
+            "expandable": true,
+            "items": "techDetails"
+        },
+        {
+            "key": "api",
+            "type": "hidden"
+        },
+        {
+            "type": "submit",
+            "title": "OK - give it to me!"
+        }
+    ];
+
+	// ---
+
+	// finally return the "handleSchema"-function-body
+	return function ( schema )
+	{
+		console.log(schema);
+
+		schema.form = formTemplate;
+		schema.onSubmit = handleSubmit;
+
+		$('form').jsonForm( schema );
+
+		currentSchema = schema;
 	};
+}();
 
-	$('form').jsonForm(schema);
-} );
+$.getJSON( "0.1.json", handleSchema );
