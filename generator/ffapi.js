a99438b722fdab6a4c84dc199c472e344149a570
Alexander Wunschik <alex@wunschik.net>
2013-09-30 00:41:19 +0200

    fixed code formating

--- generator/ffapi.js
+++ generator/ffapi.js
@@ -1,161 +1,161 @@
 var handleSchema = function()
 {
-	var currentSchema;
+  var currentSchema;
 
-	// --
+  // --
 
-	$( '.autohide' ).hide();
+  $( '.autohide' ).hide();
 
-	// ---
+  // ---
 
-	var showError =
-		function ( title, message ) {
-			$( '#error' ).show();
-			$( '#error .message' ).text( title );
-			$( '#error .details' ).text( message );
-		};
+  var showError =
+    function ( title, message ) {
+      $( '#error' ).show();
+      $( '#error .message' ).text( title );
+      $( '#error .details' ).text( message );
+    };
 
-	// ---
+  // ---
 
-	var takeJson =
-		function() {
-			$( '.autohide' ).hide();
-			var json = $( '#jsonText' ).val();
-			try {
-				currentSchema.value = JSON.parse( json );
-				$('form').empty().jsonForm( currentSchema );
-			}
-			catch ( e ) {
-				showError( "JSON Syntax Error", e );
-			}
-		};
+  var takeJson =
+    function() {
+      $( '.autohide' ).hide();
+      var json = $( '#jsonText' ).val();
+      try {
+        currentSchema.value = JSON.parse( json );
+        $('form').empty().jsonForm( currentSchema );
+      }
+      catch ( e ) {
+        showError( "JSON Syntax Error", e );
+      }
+    };
 
-	// ---
+  // ---
 
-	$( '#jsonTakeButton').bind( 'click', takeJson );
+  $( '#jsonTakeButton').bind( 'click', takeJson );
 
-	// ---
-	
-	var dirSelect = 
-		function() {
-			$( '#dirselect' ).append($('<option>').text('choose a community from list'));
-			$.getJSON( "php-simple-proxy/ba-simple-proxy.php?url=https://raw.github.com/freifunk/api.freifunk.net/master/directory/directory.json", function(dir) {
-				$.each( dir.contents, function (key, val) {
-					$( '#dirselect' )
-						.append($('<option>', { value : val })
-					        .text(key));
-				});
-			}); 
-		};
+  // ---
+  
+  var dirSelect = 
+    function() {
+      $( '#dirselect' ).append($('<option>').text('choose a community from list'));
+      $.getJSON( "php-simple-proxy/ba-simple-proxy.php?url=https://raw.github.com/freifunk/api.freifunk.net/master/directory/directory.json", function(dir) {
+        $.each( dir.contents, function (key, val) {
+          $( '#dirselect' )
+            .append($('<option>', { value : val })
+              .text(key));
+        });
+      }); 
+    };
 
-	// ---
+  // ---
 
-	$( '#dirselect' ).on('change', function() {
-		$.getJSON( "php-simple-proxy/ba-simple-proxy.php?url=" + this.value, function(communityJson) {
-			$( '#jsonText' )
-				.empty()
-				.val(JSON.stringify( communityJson.contents, null, '  '));
-		});
-	});
+  $( '#dirselect' ).on('change', function() {
+    $.getJSON( "php-simple-proxy/ba-simple-proxy.php?url=" + this.value, function(communityJson) {
+      $( '#jsonText' )
+        .empty()
+        .val(JSON.stringify( communityJson.contents, null, '  '));
+    });
+  });
 
-	// ---
-	
-	var validate = 
-		function(errors, values) {
-			takeJson();
-			handleSubmit();
-		};
+  // ---
+  
+  var validate = 
+    function(errors, values) {
+      takeJson();
+      handleSubmit();
+    };
 
-	// ---
+  // ---
 
-	$( '#validateButton').bind( 'click', validate);
-	
-	// ---
+  $( '#validateButton').bind( 'click', validate);
+  
+  // ---
 
-	var download = 
-		function() {
-			window.location = "data:application/text,"+ $( '#jsonText' ).val();
-		};
+  var download = 
+    function() {
+      window.location = "data:application/text,"+ $( '#jsonText' ).val();
+    };
 
-	$( '#downloadButton').bind( 'click', download);
+  $( '#downloadButton').bind( 'click', download);
 
-	// ---
+  // ---
 
-	var handleSubmit =
-		function (errors, values) {
-	        $( '.autohide' ).hide();
-	        if (errors) {
-	            showError( 'I beg your pardon?', JSON.stringify( errors ) );
-	        }
-	        else {
-	            $( '#result .message' ).show().text( 'Hello ' + values.name + '. This is your API file. Place it on a public webserver and add the URL to our directory.' );
-	            $( '#jsonText' ).val( JSON.stringify( values, null, '  ' ) );
-	            $( 'body' ).scrollTop( 0 );
-	        }
-	    };
+  var handleSubmit =
+    function (errors, values) {
+      $( '.autohide' ).hide();
+      if (errors) {
+        showError( 'I beg your pardon?', JSON.stringify( errors ) );
+      }
+      else {
+        $( '#result .message' ).show().text( 'Hello ' + values.name + '. This is your API file. Place it on a public webserver and add the URL to our directory.' );
+        $( '#jsonText' ).val( JSON.stringify( values, null, '  ' ) );
+        $( 'body' ).scrollTop( 0 );
+      }
+    };
 
-	// ---
+  // ---
 
-	var formTemplate = [
-        "name",
-        "metacommunity",
-        "url",
-        "state",
-        {
-            "type": "fieldset",
-            "expanded": true,
-            "items": "location"
-        },
-        {
-            "type": "fieldset",
-            "title":"Contact (one required)",
-            "expandable": true,
-            "items": "contact"
-        },
-        {
-            "type": "fieldset",
-            "title":"Events",
-            "expandable": true,
-            "items": "events"
-        },
-        {
-            "type": "fieldset",
-            "title":"Feeds",
-            "expandable": true,
-            "items": "feeds"
-        },
-        {
-            "type": "fieldset",
-            "title":"Technical Details",
-            "expandable": true,
-            "items": "techDetails"
-        },
-        {
-            "key": "api",
-            "type": "hidden"
-        },
-        {
-            "type": "submit",
-            "title": "OK - give it to me!"
-        }
-    ];
+  var formTemplate = [
+    "name",
+    "metacommunity",
+    "url",
+    "state",
+    {
+      "type": "fieldset",
+      "expanded": true,
+      "items": "location"
+    },
+    {
+      "type": "fieldset",
+      "title":"Contact (one required)",
+      "expandable": true,
+      "items": "contact"
+    },
+    {
+      "type": "fieldset",
+      "title":"Events",
+      "expandable": true,
+      "items": "events"
+    },
+    {
+      "type": "fieldset",
+      "title":"Feeds",
+      "expandable": true,
+      "items": "feeds"
+    },
+    {
+      "type": "fieldset",
+      "title":"Technical Details",
+      "expandable": true,
+      "items": "techDetails"
+    },
+    {
+      "key": "api",
+      "type": "hidden"
+    },
+    {
+      "type": "submit",
+      "title": "OK - give it to me!"
+    }
+  ];
 
-	// ---
+  // ---
 
-	// finally return the "handleSchema"-function-body
-	return function ( schema )
-	{
-		console.log(schema);
+  // finally return the "handleSchema"-function-body
+  return function ( schema )
+  {
+    console.log(schema);
 
-		schema.form = formTemplate;
-		schema.onSubmit = handleSubmit;
+    schema.form = formTemplate;
+    schema.onSubmit = handleSubmit;
 
-		directory = dirSelect();
+    directory = dirSelect();
 
-		$('form').jsonForm( schema );
+    $('form').jsonForm( schema );
 
-		currentSchema = schema;
-	};
+    currentSchema = schema;
+  };
 }();
 
 $.getJSON( "0.1.json", handleSchema );
